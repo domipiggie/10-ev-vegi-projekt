9906f59269133e5be74de71f4f667c49471652b4
Dominik <suge.dominik.2020i@bankitatabanya.hu>
2022-06-06 17:36:20 +0200

    könyvlista msql ,megjelenítés

--- ViewBooks.py
+++ ViewBooks.py
@@ -36,7 +36,17 @@ def View():
     labelFrame.place(relx=0.1,rely=0.3,relwidth=0.8,relheight=0.5)
     y = 0.25
     
-    # TODO adatbázisból lekérni az összes könyvet, azokat meg valahogy megjeleníteni majd írj dc-n és megbezéljük
+    Label(labelFrame, text="%-10s%-40s%-30s%-20s"%('BID','Title','Author','Status'),bg='black',fg='white').place(relx=0.07,rely=0.1)
+    Label(labelFrame, text="----------------------------------------------------------------------------",bg='black',fg='white').place(relx=0.05,rely=0.2)
+    getBooks = "select * from "+bookTable
+    try:
+        cur.execute(getBooks)
+        con.commit()
+        for i in cur:
+            Label(labelFrame, text="%-10s%-30s%-30s%-20s"%(i[0],i[1],i[2],i[3]),bg='black',fg='white').place(relx=0.07,rely=y)
+            y += 0.1
+    except:
+        messagebox.showinfo("Failed to fetch files from database")
     
     quitBtn = Button(root,text="Quit",bg='#f7f1e3', fg='black', command=root.destroy)
     quitBtn.place(relx=0.4,rely=0.9, relwidth=0.18,relheight=0.08)
