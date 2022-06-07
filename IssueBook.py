ac30d839d4d23d9724685af32e28bd00bcd69116
Dominik <suge.dominik.2020i@bankitatabanya.hu>
2022-06-08 00:07:18 +0200

    könyv kiosztás mysql

--- IssueBook.py
+++ IssueBook.py
@@ -18,8 +18,67 @@ bookTable = "books"
 allBid = [] 
 
 def issue():
-    # sql kód idk pls valami majd dc-n kommunikáljuk hogy hogy mit
-    print("")
+    
+    global issueBtn,labelFrame,lb1,inf1,inf2,quitBtn,root,Canvas1,status
+    
+    bid = inf1.get()
+    issueto = inf2.get()
+
+    issueBtn.destroy()
+    labelFrame.destroy()
+    lb1.destroy()
+    inf1.destroy()
+    inf2.destroy()
+    
+    
+    extractBid = "select bid from "+bookTable
+    try:
+        cur.execute(extractBid)
+        con.commit()
+        for i in cur:
+            allBid.append(i[0])
+        
+        if bid in allBid:
+            checkAvail = "select status from "+bookTable+" where bid = '"+bid+"'"
+            cur.execute(checkAvail)
+            con.commit()
+            for i in cur:
+                check = i[0]
+                
+            if check == 'avail':
+                status = True
+            else:
+                status = False
+
+        else:
+            messagebox.showinfo("Error","Book ID not present")
+    except:
+        messagebox.showinfo("Error","Can't fetch Book IDs")
+    
+    issueSql = "insert into "+issueTable+" values ('"+bid+"','"+issueto+"')"
+    show = "select * from "+issueTable
+    
+    updateStatus = "update "+bookTable+" set status = 'issued' where bid = '"+bid+"'"
+    try:
+        if bid in allBid and status == True:
+            cur.execute(issueSql)
+            con.commit()
+            cur.execute(updateStatus)
+            con.commit()
+            messagebox.showinfo('Success',"Book Issued Successfully")
+            root.destroy()
+        else:
+            allBid.clear()
+            messagebox.showinfo('Message',"Book Already Issued")
+            root.destroy()
+            return
+    except:
+        messagebox.showinfo("Search Error","The value entered is wrong, Try again")
+    
+    print(bid)
+    print(issueto)
+    
+    allBid.clear()
     
 def issueBook(): 
     
