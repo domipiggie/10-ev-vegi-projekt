2ae7f4414d5451fee2f03eba166a45d8b536e28f
Dominik <suge.dominik.2020i@bankitatabanya.hu>
2022-06-08 12:28:49 +0200

    könyv visszaadás mysql

--- ReturnBook.py
+++ ReturnBook.py
@@ -17,7 +17,60 @@ bookTable = "books"
 allBid = []
 
 def returnn():
-    print("no")
+    
+    global SubmitBtn,labelFrame,lb1,bookInfo1,quitBtn,root,Canvas1,status
+    
+    bid = bookInfo1.get()
+
+    extractBid = "select bid from "+issueTable
+    try:
+        cur.execute(extractBid)
+        con.commit()
+        for i in cur:
+            allBid.append(i[0])
+        
+        if bid in allBid:
+            checkAvail = "select status from "+bookTable+" where bid = '"+bid+"'"
+            cur.execute(checkAvail)
+            con.commit()
+            for i in cur:
+                check = i[0]
+                
+            if check == 'issued':
+                status = True
+            else:
+                status = False
+
+        else:
+            messagebox.showinfo("Error","Book ID not present")
+    except:
+        messagebox.showinfo("Error","Can't fetch Book IDs")
+    
+    
+    issueSql = "delete from "+issueTable+" where bid = '"+bid+"'"
+  
+    print(bid in allBid)
+    print(status)
+    updateStatus = "update "+bookTable+" set status = 'avail' where bid = '"+bid+"'"
+    try:
+        if bid in allBid and status == True:
+            cur.execute(issueSql)
+            con.commit()
+            cur.execute(updateStatus)
+            con.commit()
+            messagebox.showinfo('Success',"Book Returned Successfully")
+        else:
+            allBid.clear()
+            messagebox.showinfo('Message',"Please check the book ID")
+            root.destroy()
+            return
+    except:
+        messagebox.showinfo("Search Error","The value entered is wrong, Try again")
+    
+    
+    allBid.clear()
+    root.destroy()
+
     
 def returnBook(): 
     
